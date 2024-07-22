4a351b1c0429b797d9a3112984a0ae73e52ea06d
justinakanbi <justinakanbi22@gmail.com>
2024-07-22 16:13:14 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+let adviceId = document.querySelector(".adv");
+let adviceContent = document.querySelector(".quo");
+let loading = document.querySelector(".loading");
+let button = document.querySelector(".glow");
+
+
+let adviceData = async () => {
+let response = await fetch("https://api.adviceslip.com/advice")
+let data = await response.json();
+loading.classList.add("d-none")
+adviceId.textContent = `ADVICE #${data.slip.id}`;
+adviceContent.textContent = `${data.slip,advice}`;
+};
+
+button.addEventListener("click", () => {
+    location.reload();
+})
+
+setTimeout(() => {
+    adviceData();
+}, 3000)
